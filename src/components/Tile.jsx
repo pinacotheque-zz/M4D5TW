4dd8246573b677cc2e0f2f735d7a4689869c118a
Gamze Güneri <62291935+pinacotheque@users.noreply.github.com>
2021-06-04 15:04:21 +0300

    picture added

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,14 +1,14 @@
 import {Component} from 'react'
-
+import Thumbnail from "../assets/media/media32.jpg"
 class Tile extends Component {
     render(){
         return(
 
-            
+
             
             (<div className="tile">
                 <div className="tile__media">
-                  <img className="tile__img" src="assets/media/media32.jpg" alt=""  />
+                  <img className="tile__img" src={Thumbnail} alt=""  />
                 </div>
                 <div className="tile__details">
                   <div className="tile__title">
