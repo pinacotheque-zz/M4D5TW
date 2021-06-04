65fd00e84e901209d1c2054d97b212b7c639379b
Aizada <Aizakoni29@gmail.com>
2021-06-04 14:13:23 +0100

    Navbar Footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import {Badge} from 'react-bootstrap'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNav from './component/Navbar'
+import MyFooter from './component/Footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+        <MyNav />
+      <MyFooter/>
     </div>
   );
 }
